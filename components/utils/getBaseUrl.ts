acff0c2a83978e00cdbbc21d8ac745593d2de768
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-03-02 21:58:55 +0530

    fixes for base url

--- components/utils/getBaseUrl.ts
+++ components/utils/getBaseUrl.ts
@@ -2,13 +2,15 @@ export const getBaseUrl = (): string => {
   const prodUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   const vercelUrl = process.env.VERCEL_URL
 
+  console.log('prodUrl', prodUrl, 'vercelUrl', vercelUrl)
+
   if (prodUrl) {
-    return prodUrl.startsWith('http') ? prodUrl : `https://${prodUrl}`
+    return `https://${prodUrl}`
   }
 
   if (vercelUrl) {
     return `https://${vercelUrl}`
   }
 
-  return 'http://localhost:3000'
+  return 'http://localhost:3000' // Local fallback
 }
