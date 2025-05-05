4530cdf43098bd2ce48e33fff6ef427f043b15d6
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-05-06 01:47:14 +0530

    fix: remove Google Analytics integration and retain Google Tag Manager

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -5,7 +5,7 @@ import SkipToContent from '@/ui/SkipToContent'
 import VisualEditingControls from '@/ui/VisualEditingControls'
 import Footer from '@/ui/footer'
 import Header from '@/ui/header'
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import _ from 'next/dynamic'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
@@ -35,7 +35,6 @@ export default async function RootLayout({
 					<VisualEditingControls />
 				</NuqsAdapter>
 
-				<GoogleAnalytics gaId={process.env.NEXT_GOOGLE_ANALYTICS_ID || ''} />
 				<SpeedInsights />
 			</body>
 		</Root>
