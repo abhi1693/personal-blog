5cd8f1d35c2650b7be2455d0e6e1ad0dcd3d51f8
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-05-18 21:14:56 +0530

    refactor(env): format BASE_URL assignment for improved readability

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 export const dev = process.env.NODE_ENV === 'development'
 export const vercelPreview = process.env.VERCEL_ENV === 'preview'
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+export const BASE_URL =
+	process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
 
 export const BLOG_DIR = 'posts'
