8b541df8f1ba20e9c156aace42dfd8f0ceee9ecb
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-05-20 19:21:51 +0530

    refactor(getLang): update language extraction regex to include dynamic blog directory

--- src/lib/getLang.ts
+++ src/lib/getLang.ts
@@ -1,5 +1,6 @@
 'use client'
 
+import { BLOG_DIR } from '@/lib/env'
 import { languages } from '@/lib/i18n'
 import { usePathname } from 'next/navigation'
 
@@ -7,8 +8,9 @@ export default function useLang() {
 	const pathname = usePathname()
 
 	const { lang } =
-		pathname.match(new RegExp(`^/(blog/)?(?<lang>[${languages.join('|')}]+)`))
-			?.groups ?? {}
+		pathname.match(
+			new RegExp(`^/(${BLOG_DIR}/)?(?<lang>[${languages.join('|')}]+)`),
+		)?.groups ?? {}
 
 	return lang || languages?.[0] || 'en'
 }
